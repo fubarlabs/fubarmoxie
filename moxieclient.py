12cab52ee26b911bac3902b4d862d407c93445c9
Jackie <gersteinj@gmail.com>
2019-10-24 22:36:33 -0400

    loop allows other things to happen

--- moxieclient.py
+++ moxieclient.py
@@ -1,5 +1,8 @@
+# https://mntolia.com/mqtt-python-with-paho-mqtt-client/ was very helpful
+
 import paho.mqtt.client as mqtt
 import time
+
 
 broker_url = "pi-iot.local"
 broker_port = 1883
@@ -9,12 +12,15 @@ def on_connect(client, userdata, flags, rc):
 
 def on_disconnect(client, userdata, rc):
     print("Client got disconnected")
+    # client.loop_stop()
 
 def on_button_message(client, userdata, message):
-    print("Buttons!", message.payload.decode())
-
-# def on_message(client, userdata, message):
-#     print(message.payload.decode())
+    buttons = message.payload.decode()
+    print("Buttons!", buttons)
+     
+def on_message(client, userdata, message):
+    values = message.payload.decode()
+    print(message.payload.decode())
 
 client = mqtt.Client("robotsinheels")
 client.on_connect = on_connect
@@ -22,8 +28,10 @@ client.on_disconnect = on_disconnect
 
 # client.on_message = on_message
 client.connect(broker_url, broker_port)
-
+client.loop_start()
 client.subscribe("BtnsOut", qos=1)
-client.message_callback_add("BtnsOut", on_button_message)
 
-client.loop_forever()
+while True:
+    # client.message_callback_add("BtnsOut", on_button_message)
+    client.on_message = on_message
+# client.loop_forever()
