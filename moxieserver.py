79379db1bb3f8212dfb4d95e9372657844545f86
rianders <rianders@docs.rutgers.edu>
2019-10-24 23:51:49 -0400

    Starter files for moxie server using Flask

---
+++ moxieserver.py
@@ -0,0 +1,71 @@
+import eventlet
+import json
+from flask import Flask
+from flask import render_template
+from flask_mqtt import Mqtt
+from flask_bootstrap import Bootstrap
+from flask_socketio import SocketIO
+
+
+import base64 
+import io
+
+#from flask import render_template
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+plt.switch_backend('Agg')
+
+eventlet.monkey_patch()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['MQTT_BROKER_URL'] = 'pi-iot.local'  
+app.config['MQTT_BROKER_PORT'] = 1883  # default port for non-tls connection
+app.config['MQTT_KEEPALIVE'] = 5  # set the time interval for sending a ping to the broker to 5 seconds
+
+mqtt = Mqtt(app)
+socketio = SocketIO(app)
+bootstrap = Bootstrap(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@socketio.on('publish')
+def handle_publish(json_str):
+    data = json.loads(json_str)
+    mqtt.publish(data['topic'], data['message'])
+
+
+@socketio.on('subscribe')
+def handle_subscribe(json_str):
+    data = json.loads(json_str)
+    mqtt.subscribe(data['BtnsOut'])
+
+
+@socketio.on('unsubscribe_all')
+def handle_unsubscribe_all():
+    mqtt.unsubscribe_all()
+
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    data = dict(
+        topic=message.topic,
+        payload=message.payload.decode()
+    )
+    socketio.emit('mqtt_message', data=data)
+
+
+@mqtt.on_log()
+def handle_logging(client, userdata, level, buf):
+    print(level, buf)
+
+
+if __name__ == '__main__':
+    # important: Do not use reloader because this will create two Flask instances.
+    # Flask-MQTT only supports running with one instance
+    socketio.run(app, host='0.0.0.0', port=5000, use_reloader=False, debug=False)
