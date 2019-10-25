2405c5fc20825a183970095331884731ecba6613
Jackie <gersteinj@gmail.com>
2019-10-24 22:12:58 -0400

    minor udpates

--- moxieclient.py
+++ moxieclient.py
@@ -10,15 +10,20 @@ def on_connect(client, userdata, flags, rc):
 def on_disconnect(client, userdata, rc):
     print("Client got disconnected")
 
-def on_message(client, userdata, message):
-    print(message.payload.decode())
+def on_button_message(client, userdata, message):
+    print("Buttons!", message.payload.decode())
+
+# def on_message(client, userdata, message):
+#     print(message.payload.decode())
 
 client = mqtt.Client("robotsinheels")
 client.on_connect = on_connect
 client.on_disconnect = on_disconnect
-client.on_message = on_message
+
+# client.on_message = on_message
 client.connect(broker_url, broker_port)
 
 client.subscribe("BtnsOut", qos=1)
+client.message_callback_add("BtnsOut", on_button_message)
 
 client.loop_forever()
